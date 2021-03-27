0ea5d6c5153f7b9f1b87e5b9f1c1773f3bebbbed
acisliver <7273322@naver.com>
2021-03-27 19:28:56 +0900

    DI 수정

---
+++ userdao/src/main/java/kr/ac/jejunu/demo/UserDao.java
@@ -0,0 +1,58 @@
+package kr.ac.jejunu.demo;
+
+import java.sql.*;
+
+public class UserDao {
+    private final ConnectionMaker connectionMaker;
+
+    public UserDao(ConnectionMaker connectionMaker){
+        this.connectionMaker = connectionMaker;
+    }
+
+    public User findById(Integer id) throws SQLException, ClassNotFoundException {
+        Connection connection = connectionMaker.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "select * from userinfo where id = ?"
+        );
+        preparedStatement.setInt(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        //DB 연결
+        Connection connection = connectionMaker.getConnection();
+
+        //SQL문 생성
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "insert into userinfo (name, password) values ( ?, ? )"
+                ,Statement.RETURN_GENERATED_KEYS
+        );
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+
+        //SQL 실행
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        user.setId(resultSet.getInt(1));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+}
