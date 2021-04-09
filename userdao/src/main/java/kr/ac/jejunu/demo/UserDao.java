68bcf58a5e2c80d3a661f735de69236f0ce088b4
acisliver <7273322@naver.com>
2021-04-10 05:00:52 +0900

    Template/Callback Pattern
    람다식을 사용해 콜백함수로 Context 구현

--- userdao/src/main/java/kr/ac/jejunu/demo/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/demo/UserDao.java
@@ -10,23 +10,51 @@ public class UserDao {
     }
 
     public User findById(Integer id) throws SQLException {
-        StatementStrategy statementStrategy = new FindByIdStatementStrategy(id);
-        return jdbcContext.jdbcContextForFindById(statementStrategy);
+        return jdbcContext.jdbcContextForFindById(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select * from userinfo where id = ?"
+            );
+            preparedStatement.setInt(1, id);
+            return preparedStatement;
+        });
     }
 
     public void insert(User user) throws SQLException {
-        StatementStrategy statementStrategy = new InsertStatementStrategy(user);
-        jdbcContext.jdbcContextInsert(user, statementStrategy);
+        jdbcContext.jdbcContextInsert(user, connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "insert into userinfo (name, password) values ( ?, ? )"
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            return preparedStatement;
+        });
     }
 
     public void update(User user) throws SQLException {
-        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
-        jdbcContext.jdbcContextForUpdate(statementStrategy);
+        jdbcContext.jdbcContextForUpdate(connection -> {
+            PreparedStatement preparedStatement =connection.prepareStatement(
+                    "update userinfo set name = ?, password = ? where id = ?"
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getId());
+            return preparedStatement;
+        });
     }
 
     public void delete(Integer id) throws SQLException {
-        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
-        jdbcContext.jdbcContextForDelete(statementStrategy);
+        jdbcContext.jdbcContextForDelete(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "delete from userinfo where id = ?"
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+            return preparedStatement;
+        });
     }
 
 }
